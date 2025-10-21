cbf1b0cf420ab0fc329ac6effc2dcced11e0da11
thedrew94 <andrewlaurentiu94@gmail.com>
2025-10-21 19:11:17 +0200

    initial setup and styles setup

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  content: ["./app/*/.{js,ts,jsx,tsx,mdx}"],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#1D4ED8",
+      },
+    },
+  },
+  plugins: [],
+};
