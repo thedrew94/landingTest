f5728411fceb8cbd26d58ab747e49bcca37fd9fe
thedrew94 <andrewlaurentiu94@gmail.com>
2025-10-21 21:28:59 +0200

    added generic html structure for future components

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -3,9 +3,29 @@ import Navbar from "../_components/Navbar";
 
 export default function Page() {
   return (
-    <header className="relative p-[16px] bg-background">
-      <Navbar />
-      <Hero />
-    </header>
+    <>
+      <header className="relative p-[16px] bg-background">
+        <Navbar />
+        <Hero />
+      </header>
+      <main className="bg-[rgba(0, 0, 0, 1)]">
+        {/* INTRO SECTION */}
+        <div>
+          <h2>Il laboratorio urbano della Città di Lugano.</h2>
+          <h3>
+            Promuoviamo e facilitiamo l'innovazione digitale e tecnologica, creando, testando e implementando soluzioni
+            pensate per una città più intelligente, ma sempre a misura d’uomo.
+          </h3>
+        </div>
+        {/* CTA SECTION */}
+        <div></div>
+        {/* FEATURED SECTION */}
+        <div></div>
+        {/* ANIMATED HORIZONTAL SLIDER SECTION */}
+        <div></div>
+      </main>
+      {/* FOOTER */}
+      <footer></footer>
+    </>
   );
 }
