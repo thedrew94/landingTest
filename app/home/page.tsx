ba7d12f9b1167758b91dfeaf7dea8cdb6f0a9b8f
thedrew94 <andrewlaurentiu94@gmail.com>
2025-10-24 09:13:23 +0200

    added responsive to the cta section

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -40,12 +40,19 @@ export default function Page() {
           <h2 className="text-right xl:text-left xl:translate-x-[678px] text-h9 xl:text-h2">Diffondiamo</h2>
         </div>
         {/* FEATURED SECTION */}
-        <div className="py-[119px]">
+        <div className="py-[148.5px] xl:py-[119px]">
           {/* TOP ROW */}
-          <div className="flex justify-between items-baseline-last pt-[16px] pb-[32px] px-[16px]">
+          <div className="hidden xl:flex justify-between items-baseline-last pt-[16px] pb-[32px] px-[16px]">
             <p className="text-p">Progetti 8</p>
             <h3 className="text-h3">L*3 in evidenza</h3>
             <p className="text-p">[ Tutti i progetti ]</p>
+          </div>
+          <div className="flex flex-col gap-[24px] pb-[32px] pt-[16px] px-[16px] xl:hidden">
+            <h3 className="text-h7">L*3 in evidenza</h3>
+            <div className="flex justify-between items-center w-full">
+              <p className="text-p">Progetti 8</p>
+              <p className="text-p">[ Tutti i progetti ]</p>
+            </div>
           </div>
           {/* SLIDER */}
           <Slider />
