798f2285a4b147cdcabac0fd88083025e53a5522
thedrew94 <andrewlaurentiu94@gmail.com>
2025-10-24 09:34:50 +0200

    refacto styles

--- app/_components/Navbar.tsx
+++ app/_components/Navbar.tsx
@@ -108,7 +108,7 @@ export default function Navbar() {
       <nav className="absolute top-0 left-0 z-2 w-full h-[64px] flex justify-between items-center gap-4 bg-[rgba(0,0,0,0.28)] backdrop-blur-xl border-nav">
         {/* TOP NAVIGATION BAR CONTENT */}
         <div className="h-full flex items-center gap-[16px]">
-          <div className="w-[285px] max-w-[285px] h-full flex items-center">
+          <div className="w-[180px] max-w-[180px] xl:w-[285px] xl:max-w-[285px] h-full flex items-center">
             <button
               className="relative z-3 w-[64px] h-full flex justify-center items-center bg-[#6633FF] border-none outline-none cursor-pointer"
               onClick={handleMenu}
@@ -136,12 +136,14 @@ export default function Navbar() {
           </ul>
         </div>
         <div className="h-full flex items-center gap-[16px]">
-          <LanguageSelector />
+          <div className="hidden xl:block">
+            <LanguageSelector />
+          </div>
           <a
             href="#"
             target="_blank"
             rel="noopener noreferrer"
-            className="w-[144px] h-full flex justify-center items-center gap-[4px] bg-[#FFFFFF47] bg-opacity-[28] backdrop-blur-[24]"
+            className="block w-[144px] max-w-[144px] h-full flex justify-center items-center gap-[4px] bg-[#FFFFFF47] bg-opacity-[28] backdrop-blur-[24]"
           >
             <p className="text-h5">Contatto</p>
             <span className="w-[32px] h-[32px] flex justify-center items-center">
